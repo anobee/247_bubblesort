376c7385b5726e4c9725e90df96df72494e38f55
dicky <161241858+anobee@users.noreply.github.com>
2024-03-07 20:00:20 +0700

    bubblesort.cpp

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -1,20 +1,78 @@
 // bubblesort.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+
+        }
+    }
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "masukan Elemen Array" << endl;
+    cout << "===============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+
+    }
+
+}
+
+void bubbleSortArray() { //procedur untuk mengurutkan array dengan metode bublle sort
+
+    int pass = 1; // step 1
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) { // step 2
+            if (arr[j] > arr[j + 1]) { //step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+
+            }
+        }
+        pass = pass + 1; //step 4
+
+    } while (pass <= n); //step 5
+}
+
+void display() {
+    cout << endl;
+    cout << "===================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "===================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; // Output setiap element array pada garis baru
+
+    }
+    cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
+    cout << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    input();  //memanggil prosedur untuk menginput data
+    bubbleSortArray();  //memanggil prosedur bubblesort untuk proses mengurutkan data
+    display();//memanggil prosedur untuk menampilkan data yang telah urut
+    system("pause");
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
